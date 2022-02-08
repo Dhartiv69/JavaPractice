dcf35813ee4a224c3cd2265cdad73d74eec1556a
DHARTI <dhartiv69@gmail.com>
2022-02-08 13:59:27 -0500

    Branch_02 first commit

---
+++ src/Test7.java
@@ -0,0 +1,30 @@
+
+public class Test7 {
+
+	public static void main(String[] args) {
+		
+		boolean d = 100==100;
+		System.out.println("100 Equal to 100 is "+d);
+		
+		d = 50!=50;
+		System.out.println("50 Not equal to 50 is "+d);
+		
+		d = 25>50;
+		System.out.println("25 Greater than 50 is "+d);
+		
+		d = 25<50;
+		System.out.println("25 Less than 50 is "+d);
+		
+		d = 50>=30;
+		System.out.println("50 Greater than or equal to 30 is "+d);
+		
+		d = 40<=30;
+		System.out.println("40 Less than or equal to 30 is "+d);
+		
+		
+		
+		
+
+	}
+
+}
